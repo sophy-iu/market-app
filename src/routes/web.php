b561ac8b15001ee918f4c5071887570866eac3bf
sophy-iu <phyro8835@icloud.cpm>
2026-05-29 23:34:54 +0900

    first commit

---
+++ src/routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ItemController;
+use App\Http\Controllers\PurchaseController;
+use App\Http\Controllers\AddressController;
+use App\Http\Controllers\ProfileController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', [ItemController::class, 'index']);
+Route::get('/item/{item_id}', [ItemController::class, 'detail']);
+
+Route::middleware('auth')->group(function () {
+    Route::get('/purchase/{item_id}', [PurchaseController::class, 'detail']);
+    Route::get('/sell', [ItemController::class, 'sell']);
+
+    Route::get('/purchase/address/{item_id}', [AddressController::class, 'address']);
+
+    Route::get('/mypage', [ProfileController::class, 'profile']);
+    Route::get('/mypage/profile', [ProfileController::class, 'edit']);
+});
