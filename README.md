0cf7e88acb72a856971d3c2a466d2ea7b33c6c02
sophy-iu <phyro8835@icloud.cpm>
2026-05-30 01:33:47 +0900

    マイグレーションまで完了

--- README.md
+++ README.md
@@ -1,1 +1,58 @@
-# market-app
+# COACHTECH(フリマアプリ)
+
+## 環境構築
+**Dockerビルド**
+1. `git clone git@github.com:sophy-iu/market-app.git`
+2.  cd market-app
+3.  DockerDesktopアプリを立ち上げる
+4. `docker-compose up -d --build`
+
+> *MacのM1・M2チップのPCの場合、`no matching manifest for linux/arm64/v8 in the manifest list entries`のメッセージが表示されビルドができないことがあります。
+エラーが発生する場合は、docker-compose.ymlファイルの「mysql」内に「platform」の項目を追加で記載してください*
+``` bash
+mysql:
+    platform: linux/x86_64(この文追加)
+    image: mysql:8.0.26
+    environment:
+```
+
+**Laravel環境構築**
+1. `docker-compose exec php bash`
+2. `composer install`
+3. 「.env.example」ファイルを 「.env」ファイルに命名を変更。または、新しく.envファイルを作成
+4. .envに以下の環境変数を追加
+``` text
+DB_CONNECTION=mysql
+DB_HOST=mysql
+DB_PORT=3306
+DB_DATABASE=laravel_db
+DB_USERNAME=laravel_user
+DB_PASSWORD=laravel_pass
+```
+5. アプリケーションキーの作成
+``` bash
+php artisan key:generate
+```
+
+6. マイグレーションの実行
+``` bash
+php artisan migrate
+```
+
+7. シーディングの実行
+``` bash
+php artisan db:seed
+```
+
+## 使用技術(実行環境)
+- PHP8.1.34
+- Laravel8.83.29
+- MySQL8.0.26
+- 
+
+## ER図
+![ER図](er.png)
+
+## URL
+- 開発環境：http://localhost/
+- phpMyAdmin:：http://localhost:8080/
---
+++ src/database/migrations/2026_05_30_004921_create_items_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('category_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('condition_id')->constrained()->cascadeOnDelete();
+            $table->string('image');
+            $table->string('item_name');
+            $table->string('brand_name');
+            $table->integer('price');
+            $table->integer('like_number');
+            $table->integer('comment_number');
+            $table->text('item_description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+}
